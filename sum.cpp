738c410905e824c119770515d5a2d3c55b575a60
Abhishek Gadamsetty <abhishek168421@gmail.com>
2021-07-11 12:45:10 +0530

    initial commit

---
+++ sum.cpp
@@ -0,0 +1,13 @@
+#include<iostream> 
+using namespace std;
+int sum(int x , int y){
+    int add;
+    add = x+y;
+    return add;
+}
+int main()
+ {
+    int a,b;
+    cout<<"The Sum is "<<sum(4,5);
+    return 0;
+}
