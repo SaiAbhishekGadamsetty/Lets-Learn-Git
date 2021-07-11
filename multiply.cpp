ff0e8d2828f8e897ee10502102e971c7d7d4a1e0
Abhishek Gadamsetty <abhishek168421@gmail.com>
2021-07-11 12:50:02 +0530

    multiply

---
+++ multiply.cpp
@@ -0,0 +1,14 @@
+#include<iostream> 
+using namespace std;
+int sum(int x , int y){
+    int add;
+    add = x*y;
+    return add;
+}
+int main()
+ {
+    int a,b;
+    cout<<"The Sum is "<<sum(4,5);
+    cout<<"Hello World";
+    return 0;
+}
